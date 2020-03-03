d265b62ff9b1ef442dc22f0ddfe986476152ce5e
khaliqmd9149 <abdulkhaliq.535@gmail.com>
2020-03-03 20:06:35 +0530

    My khaliq.py 3rd commit in pycharm

--- khaliq.py
+++ khaliq.py
@@ -2,3 +2,5 @@ number1 = input("First number:")
 number2 = input("Second number:")
 sum = float(number1) + float(number2)
 print("The sum of {0} and {1} is {2}" .format(number1,number2,sum))
+
+#Actually this is a test to conform that the programme is added to git or not
